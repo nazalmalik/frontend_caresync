8769cac18e952fa311f7daf786e6193bb6586516
Nazal Malik <mnazal250@gmail.com>
2026-05-05 16:11:12 +0500

    update the forgotpassword page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import API from "../api/axiosinstance";
+import API from "../api/axiosInstance";
 import { Link } from "react-router-dom";
 import "./Auth.css";
 
