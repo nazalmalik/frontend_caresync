acfde312cd2cd741879ec03073148961061eb473
Nazal Malik <mnazal250@gmail.com>
2026-05-05 11:50:36 +0500

    updated all

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./footer.css";
 
+
 const Footer = () => {
   return (
     <footer className="footer-section">
