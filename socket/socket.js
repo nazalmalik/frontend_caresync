73a9294dba31eab2a64d0f5c7620f7ad56df5ff2
Nazal Malik <mnazal250@gmail.com>
2026-05-06 23:09:18 +0500

    updated file

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://caresync-backend-production-b0da.up.railway.app/");
-
+const socket = io("https://caresync-backend-production-b0da.up.railway.app", {
+  transports: ["websocket"], // ✅ force websocket only
+  withCredentials: true
+});
 export default socket;
