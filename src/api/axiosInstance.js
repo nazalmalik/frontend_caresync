5fe1d7196241671e31a201b68446a03fd555b7c9
Nazal Malik <mnazal250@gmail.com>
2026-05-06 23:29:56 +0500

    backend url updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||"https://caresync-backend-production-b0da.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_URL ||"https://caresync-backend-production-b0da.up.railway.app",
 });
 
 axiosInstance.interceptors.request.use((config) => {
